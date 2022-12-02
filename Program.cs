11380b6716e137e321b226c5d230bef5ab9db2be
Muhammad Fadila <muh.fadila18@gmail.com>
2022-12-02 16:28:02 +0700

    Create Class Node

--- Program.cs
+++ Program.cs
@@ -2,8 +2,15 @@
 
 namespace Stack_Using_Linked_List
 {
-    class Program
+    class Node
     {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
