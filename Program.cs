cc3a0c2fb10b3ac12991356b26c3155cd27cb924
Muhammad Fadila <muh.fadila18@gmail.com>
2022-12-02 16:24:49 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stack_Using_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
